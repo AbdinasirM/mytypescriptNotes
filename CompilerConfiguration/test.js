5a7d5701e45f96ce559dfc5951078117b08d014c
Abdi <aabdi5520@gmail.com>
2024-01-07 16:44:04 -0600

    Third day of TypescriptCourse

--- CompilerConfiguration/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const norma = {
-    breed: "Silkie",
-    eggsPerWeek: 12,
-    name: 'Norma',
-    age: 1,
-};
-console.log(norma.name);
--- CompilerConfiguration/test2.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-;
-const printProduct = (product) => {
-    console.log(`Product ${product.name} - ${product.price}`);
-};
